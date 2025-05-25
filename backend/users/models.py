93736feac2e33355966688159e82f489e4ff4bde
Foksen <izholobov2004@gmail.com>
2025-05-25 21:20:06 +0300

    Init project, add users app

---
+++ backend/users/models.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    email = models.EmailField('Электронная почта', unique=True)
+    username = models.CharField('Ник', unique=True)
+    first_name = models.CharField('Имя')
+    last_name = models.CharField('Фамилия')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.username
+
+
+class Subscription(models.Model):
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='subscribers',
+    )
+    subscriber = models.ForeignKey(
+        User,
+        verbose_name='Подписчик',
+        on_delete=models.CASCADE,
+        related_name='subscriptions',
+    )
